23d206657e6a2f8ba67d214ea0712b6b4c5dfaac
Sean Ryan <seanpryan89@gmail.com>
2018-12-25 14:54:56 -0500

    completed checkpoint 8 events

--- src/App.js
+++ src/App.js
@@ -22,8 +22,10 @@ class App extends Component {
 
   handleSubmit(e) {
   e.preventDefault();
+  if (!this.state.newTodoDescription) { return }
   const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
-  this.setState({ todos: [...this.state.todos, newTodo] });  
+  this.setState( {todos: [...this.state.todos, newTodo], newTodoDescription: ''});
+
 }
 
   toggleComplete(index) {
